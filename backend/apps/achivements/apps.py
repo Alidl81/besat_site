fb9dda5475424c51089e006e5392efea9318b2a8
unknown <jalil>
2026-06-20 23:38:19 -0700

    update app app name

--- backend/apps/achivements/apps.py
+++ backend/apps/achivements/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class AchivementsConfig(AppConfig):
+    default_auto_field = "django.db.model.BigAutoField"
     name = 'apps.achivements'
+    verbose_name = "Achivements"
